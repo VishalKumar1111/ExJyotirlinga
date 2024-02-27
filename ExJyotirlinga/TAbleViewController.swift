7c9b0945b4ae7e82f0052af5620d9d41c2ff8ad7
rlogixxtraining <singh01vishal01@gmail.com>
2024-02-27 13:03:56 +0530

    tableview Select

--- ExJyotirlinga/TAbleViewController.swift
+++ ExJyotirlinga/TAbleViewController.swift
@@ -12,6 +12,7 @@ class TAbleViewController: UIViewController,UITableViewDelegate,UITableViewDataS
     
     
     @IBOutlet weak var tblView: UITableView!
+    var name = String()
     
     var arrNames = [["name":"Somnath","city": "Gujarat","image" :UIImage(named: "Somanth")],["name":"Nageshwar","city":"Maharashtra","image":UIImage(named: "Nageshwar")],[ "name":"Bhimashankar","city":"Maharashtra","image":UIImage(named: "Bhimashankar")], ["name":"Trimbakeshwar","city":"Maharashtra" ,"image":UIImage(named: "Trimbakeshwar")], ["name":"Grishneshwar","city":"Maharashtra","image":UIImage(named: "Grishneshwar")], ["name":"Vaidyanath","city":"Jharkhand","image":UIImage(named: "Vaidyanath")], ["name":"Mahakaleshwar","city":"Madhya Pradesh","image": UIImage(named: "Mahakaleshwar")], ["name":"Omkareshwar","city":"Madhya Pradesh","image":UIImage(named: "Omkareshwar")] ,["name":"Kashi Vishwanath","city":"Uttar Pradesh","image":UIImage(named: "Kashi Vishwanath")], ["name":"Kedarnath","city":"Uttarakhand","image":UIImage(named: "Kedarnath")], ["name":"Rameshwaram","city":"Tamil Nadu","image":UIImage(named: "Rameshwaram")],["name":"Mallikarjuna","city":"Andhra Pradesh","image":UIImage(named: "Mallikarjuna")]]
     override func viewDidLoad() {
@@ -35,6 +36,7 @@ class TAbleViewController: UIViewController,UITableViewDelegate,UITableViewDataS
           }
         
         cell?.textLabel?.text = arrNames[indexPath.row]["name"] as? String
+        name = cell?.textLabel?.text ?? "0"
         cell?.detailTextLabel?.text = arrNames[indexPath.row]["city"] as? String
         
         
@@ -43,7 +45,12 @@ class TAbleViewController: UIViewController,UITableViewDelegate,UITableViewDataS
     return cell!
 
     }
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let destVC = storyboard?.instantiateViewController(withIdentifier: "Detail") as! DetailViewController
+        var ab = arrNames[indexPath.row]["name"] ?? "123"
+        destVC.placeNameText = ab as! String 
+        navigationController?.pushViewController(destVC, animated: true)
+    }
     
     
     
